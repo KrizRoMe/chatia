ae0e5f100addc2fb8b4acc78b2cd4b0e4621654c
KrizRoMe <cmromero1231@gmail.com>
2025-03-06 09:01:37 -0500

    style(project): :nail_care: configure fonts

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Fira_Code } from 'next/font/google'
+
+export const inter = Inter({
+  variable: '---font-inter',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const fira_code = Fira_Code({
+  variable: '---font-fira-code',
+  subsets: ['latin'],
+  display: 'swap',
+})
